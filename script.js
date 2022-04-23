df32831e525f903c90003297a19897c9c54252c2
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-04-23 14:04:41 +0200

    v

---
+++ script.js
@@ -0,0 +1,14 @@
+// let stringToNumber = prompt("insert string"); 
+// let number = parseInt(stringToNumber); 
+
+// alert(number) 
+
+//________Binary to Decimal Number_________________________________________________________ 
+
+function binToDec(bin){ 
+  
+    return parseInt(bin, 2); 
+    
+  } 
+
+  //_________________________________________________________
