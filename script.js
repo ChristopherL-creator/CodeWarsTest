8ff33bf3bfb289eab79a390dfd6ce412510bde60
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-04-24 16:27:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-// let stringToNumber = prompt("insert string"); 
-// let number = parseInt(stringToNumber); 
+// // let stringToNumber = prompt("insert string"); 
+// // let number = parseInt(stringToNumber); 
 
-// alert(number) 
+// // alert(number) 
 
-//________Binary to Decimal Number_________________________________________________________ 
+// //________Binary to Decimal Number_________________________________________________________ 
 
-function binToDec(bin){ 
+// function binToDec(bin){ 
   
-    return parseInt(bin, 2); 
-    
-  } 
+//     return parseInt(bin, 2); 
+// } 
 
-  //_________________________________________________________
+//   //______Change string to uppercase___________________________________________________ 
+
+// function makeUpperCase(str) {
+    
+//   return str.toUpperCase();
+// } 
+
+//   //_________Calculate Body Mass Index___________________________________________________ 
+
+// function bmi(weight, height) { 
+    
+//   let bmi = weight / height ** 2; 
+//   if (bmi <= 18.5){ 
+//     return "Underweight";
+//   } else if (bmi <= 25.0){ 
+//       return "Normal";
+//     } else if (bmi <=30.0){ 
+//         return "Overweight"; 
+//       } else { 
+//           return "Obese";
+//         }
+// } 
+
+//________________Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.______________________________________________ 
+
+
+
+//______________________________________________________________
